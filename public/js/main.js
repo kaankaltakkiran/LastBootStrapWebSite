2c2dee1cd3fd6d465d3f4846fabfae81cbd3d524
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-06-03 20:36:13 +0300

    Project Added

---
+++ public/js/main.js
@@ -0,0 +1,89 @@
+/* Offfcanvas OtoClose */
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for (let i = 0; i < BtnCanvas.length; i++) {
+    BtnCanvas[i].addEventListener("click", function () {
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+    });
+}
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+/* Form Validation */
+
+
+
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+/* (() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})() */
+  function checkUncheck()
+ {
+var checkbox = document.getElementById('checkboxmodal');
+if (checkbox.checked)
+{
+checkbox.checked = false;
+}
+else
+ {
+checkbox.checked = true;
+}
+ }
+
+
+/*  document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+  el.addEventListener('shown.bs.tab', () => {
+    const target = el.getAttribute('data-bs-target')
+    const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+    bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+  })
+}) */
+
+
+(() => {
+  'use strict'
+
+ 
+  const forms = document.querySelectorAll('.needs-validation')
+
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
